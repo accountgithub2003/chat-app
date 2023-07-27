f9c9c7676c27dd3ef6907d365f6a94192eaf59ec
accountgithub2003 <137156067+accountgithub2003@users.noreply.github.com>
2023-07-27 17:09:41 +0700

    thien
    add api

--- frontend/src/utils/APIRouter.js
+++ frontend/src/utils/APIRouter.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:5000";
+const host = "https://chat-app-realtime-to0w.onrender.com";
 export const registerRouter = `${host}/api/auth/register`;
 export const loginRouter = `${host}/api/auth/login`;
 export const setAvatarRouter = `${host}/api/auth/setAvatar`; 
